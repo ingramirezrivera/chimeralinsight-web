b2066ff862b60e0696f0fd9939c9792e8527cd63
ingramirezrivera <ing.ramirezrivera@gmail.com>
2025-09-22 15:21:30 -0700

    feat: carousel for desktop ready

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,3 +1,6 @@
+// src/components/BookCard.tsx
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,31 +10,32 @@ interface BookCardProps {
   amazonUrl: string;
 }
 
-export default function BookCard({
-  title,
-  imageUrl,
-  amazonUrl,
-}: BookCardProps) {
+const BookCard = ({ title, imageUrl, amazonUrl }: BookCardProps) => {
   return (
-    <div className="flex flex-col items-center justify-center p-4">
-      {/* Tarjeta del libro */}
-      <div className="w-52 h-auto rounded-lg shadow-xl overflow-hidden bg-surface">
-        <Image
-          src={imageUrl}
-          alt={title}
-          width={200}
-          height={300}
-          className="w-full h-auto object-cover"
-        />
-        <div className="p-4 flex flex-col items-center">
-          <h3 className="text-lg font-bold text-fg text-center mt-2 mb-4">
-            {title}
-          </h3>
-          <Link href={amazonUrl} className="btn btn-primary" legacyBehavior>
-            <a className="w-full text-center">Buy Now</a>
+    <div className="flex flex-col items-center justify-center ">
+      <div className="w-64 h-auto flex flex-col rounded-b-xl shadow-2xl overflow-hidden bg-surface">
+        {/* Usar relative y una altura fija para el contenedor de la imagen */}
+        <div className="w-full h-96 relative">
+          {/* Aquí está el cambio clave: usamos object-contain */}
+          <Image src={imageUrl} alt={title} fill className="object-contain" />
+        </div>
+
+        <div className="p-4 flex flex-col items-center flex-grow">
+          <div className="h-16 flex items-center justify-center flex-grow">
+            <h3 className="text-lg font-bold text-fg text-center line-clamp-2">
+              {title}
+            </h3>
+          </div>
+          <Link
+            href={amazonUrl}
+            className="bg-[#bd0000] text-white font-bold py-3 px-6 rounded-lg shadow-md hover:bg-[#a00000] transition-colors duration-300 w-full text-center hover:scale-105"
+          >
+            Buy Now
           </Link>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default BookCard;
